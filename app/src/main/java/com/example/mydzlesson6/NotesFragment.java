ddb82cc26263828328b5dc7ed74dd74b865cc624
iliy <gromov218974@mail.ru>
2021-08-19 18:47:42 +0400

    Сделал фрагмент.Подготовил изоображение для массива.

---
+++ app/src/main/java/com/example/mydzlesson6/NotesFragment.java
@@ -0,0 +1,48 @@
+package com.example.mydzlesson6;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class NotesFragment extends Fragment {
+
+    // При создании фрагмента укажем его макет
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notes, container, false);
+    }
+
+    // вызывается после создания макета фрагмента, здесь мы проинициализируем список
+    @Override
+    public void onViewCreated( View view,  Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initList(view);
+    }
+
+    // создаём список городов на экране из массива в ресурсах
+    private void initList(View view) {
+        LinearLayout layoutView = (LinearLayout)view;
+        String[] notes = getResources().getStringArray(R.array.notes);
+
+        // В этом цикле создаём элемент TextView,
+        // заполняем его значениями,
+        // и добавляем на экран.
+        // Кроме того, создаём обработку касания на элемент
+        for(int i=0; i < notes.length; i++){
+            String note = notes[i];
+            TextView tv = new TextView(getContext());
+            tv.setText(note);
+            tv.setTextSize(30);
+            layoutView.addView(tv);
+        }
+    }
+
+}
